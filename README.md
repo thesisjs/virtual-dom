42c47d23af6addaec3c1effb4e9cf234baac80c7
Kaibito <gpuzzletime@icloud.com>
2019-02-02 02:29:45 +0300

    HEAD: Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# DOMinator
+
+## Contributing
+When contributing to *DOMinator*, please open an issue first.
+
+Please note the [code of conduct](CODE_OF_CONDUCT.md), it is desired to
+follow it in all your interactions with the project.
+
+### Reporting Bugs
+When reporting a bug, please provide a minimal case of reproduction, which
+can be:
+ * a test that is broken but should not be,
+ * a Codesandbox project in which the bug occurs.
+
+### Pull Request Process
+When sending a pull request, please do not forget the following:
+ * resolve all conflicts with `master` branch,
+ * update the README.md and/or the documentation in comments according
+ to your changes,
+ * ensure the documentation build is up-to-date in `docs` folder,
+ * ensure the new functionality is covered by tests, and all tests pass.
---
+++ tests/index.test.ts
@@ -0,0 +1,602 @@
+
+type VirtualNodeStyle = string | Partial<CSSStyleDeclaration>;
+
+type VirtualNodeAttribute = string | number | boolean | VirtualNodeStyle;
+
+interface VirtualNodeAttrs {
+	[attr: string]: VirtualNodeAttribute;
+}
+
+interface VirtualNodeEvents {
+	[event: string]: EventListenerObject;
+}
+
+interface VirtualNode {
+	tag?: string;
+	attrs?: VirtualNodeAttrs;
+	events?: any;
+	children?: VirtualNode[];
+	value?: string;
+
+	dom?: Node;
+}
+
+interface VirtualTextNode extends VirtualNode {
+	value: string;
+	tag: '#';
+	dom: Text;
+}
+
+interface VirtualCommentNode extends VirtualNode {
+	value: string;
+	tag: '!';
+	dom: Comment;
+}
+
+
+const NODE_TEXT = 0;
+const NODE_COMMENT = 1;
+const NODE_HTML = 2;
+const NODE_ELEMENT = 3;
+const NODE_FRAGMENT = 4;
+
+type VirtualNodeType = typeof NODE_TEXT
+	| typeof NODE_COMMENT
+	| typeof NODE_HTML
+	| typeof NODE_ELEMENT
+	| typeof NODE_FRAGMENT;
+
+const VirtualNodeKey = Symbol('virtualNode');
+
+
+class NodeUtils {
+
+	static mounted(node: VirtualNode) {
+		return !!node.dom;
+	}
+
+	static isFragment(node: VirtualNode) {
+		return node.tag === undefined;
+	}
+
+	static isTextNode(node: VirtualNode) {
+		return node.tag === '#';
+	}
+
+	static isCommentNode(node: VirtualNode) {
+		return node.tag === '!';
+	}
+
+	static isRawHTMLNode(node: VirtualNode) {
+		return node.tag === '<';
+	}
+
+	static hasAttributes(node: VirtualNode) {
+		return !!node.attrs;
+	}
+
+	static hasChildren(node: VirtualNode) {
+		return !!node.children;
+	}
+
+	static getType(node: VirtualNode): VirtualNodeType {
+		switch (node.tag) {
+			case '#': return NODE_TEXT;
+			case '!': return NODE_COMMENT;
+			case '<': return NODE_HTML;
+			case undefined: return NODE_FRAGMENT;
+			default: return NODE_ELEMENT;
+		}
+	}
+
+	static setDOMStyle(domNode: HTMLElement, style: VirtualNodeStyle) {
+		let styleKey;
+
+		switch (typeof style) {
+			case 'string': {
+				domNode.setAttribute('style', style);
+				break;
+			}
+
+			case 'object': {
+				for (styleKey in style) {
+					domNode.style.setProperty(styleKey, style[styleKey]);
+				}
+
+				break;
+			}
+		}
+	}
+
+	static setDOMAttribute(domNode: HTMLElement, attr: string, value: VirtualNodeAttribute) {
+		if (!value) {
+			return;
+		}
+
+		switch (attr) {
+			case 'id': {
+				domNode.id = String(value);
+				break;
+			}
+
+			case 'class': {
+				domNode.classList.value = String(value);
+				break;
+			}
+
+			case 'style': {
+				NodeUtils.setDOMStyle(domNode, value as VirtualNodeStyle);
+				break;
+			}
+
+			default: {
+				domNode.setAttribute(attr, String(value));
+				break;
+			}
+		}
+	}
+
+	static connectWithDOM(node: VirtualNode) {
+		(node.dom as any)[VirtualNodeKey] = node;
+	}
+
+	static swapDOMConnection(fromNode: VirtualNode, toNode: VirtualNode) {
+		toNode.dom = fromNode.dom;
+		fromNode.dom = undefined;
+
+		NodeUtils.connectWithDOM(toNode);
+	}
+
+	static disconnectWithDOM(domNode: Node) {
+		(domNode as any)[VirtualNodeKey] = null;
+	}
+
+	static getConnectedVirtualNode(domNode: Node) {
+		return (domNode as any)[VirtualNodeKey]
+	}
+
+}
+
+
+function mountTextNode(node: VirtualTextNode) {
+	node.dom = document.createTextNode(node.value);
+	NodeUtils.connectWithDOM(node);
+}
+
+function mountCommentNode(node: VirtualCommentNode) {
+	node.dom = document.createComment(node.value);
+	NodeUtils.connectWithDOM(node);
+}
+
+function mountFragmentNode(node: VirtualNode) {
+	node.dom = document.createDocumentFragment();
+}
+
+
+function mountElementNode(node: VirtualNode) {
+	const dom = document.createElement(node.tag);
+
+	if (NodeUtils.hasAttributes(node)) {
+		let attr;
+		let value;
+
+		for (attr in node.attrs) {
+			value = node.attrs[attr];
+			NodeUtils.setDOMAttribute(dom, attr, value);
+		}
+	}
+
+	node.dom = dom;
+	NodeUtils.connectWithDOM(node);
+}
+
+
+function mountNode(node: VirtualNode) {
+	switch (NodeUtils.getType(node)) {
+		case NODE_TEXT: return mountTextNode(node as VirtualTextNode);
+		case NODE_COMMENT: return mountCommentNode(node as VirtualCommentNode);
+		case NODE_FRAGMENT: return mountFragmentNode(node);
+		case NODE_ELEMENT: return mountElementNode(node);
+	}
+}
+
+function updateTextNode(oldNode: VirtualTextNode, node: VirtualTextNode) {
+	NodeUtils.swapDOMConnection(oldNode, node);
+	node.dom.data = node.value;
+}
+
+function updateCommentNode(oldNode: VirtualCommentNode, node: VirtualCommentNode) {
+	NodeUtils.swapDOMConnection(oldNode, node);
+	node.dom.data = node.value;
+}
+
+// Assuming oldNode.tag === node.tag
+function updateElementNode(oldNode: VirtualNode, node: VirtualNode) {
+	NodeUtils.swapDOMConnection(oldNode, node);
+
+	const dom = node.dom as HTMLElement;
+	const prevAttrs = NodeUtils.hasAttributes(oldNode);
+	const nextAttrs = NodeUtils.hasAttributes(node);
+
+	let attr;
+	let value;
+	let oldValue;
+	let typeofValue;
+	let typeofOldValue;
+	let styleAttr;
+
+	if (prevAttrs && nextAttrs) {
+		// Delete old and set new attributes
+
+		// Delete unused attributes, update used values
+		for (attr in oldNode.attrs) {
+			value = node.attrs[attr];
+			oldValue = oldNode.attrs[attr];
+			typeofValue = typeof value;
+			typeofOldValue = typeof oldNode.attrs[attr];
+
+			if (oldValue && !value) {
+				dom.removeAttribute(attr);
+				continue;
+			}
+
+			if (attr === 'style') {
+				if (
+					typeofValue === 'object' &&
+					typeofOldValue === 'string'
+				) {
+					// Clear styles if we update from string to object
+					dom.removeAttribute('style');
+				} else if (
+					typeofValue === 'object' &&
+					typeofOldValue === 'object'
+				) {
+					// Clear styles if we update between objects
+					for (styleAttr in oldValue as object) {
+						if ((value as any)[styleAttr] === undefined) {
+							dom.style.removeProperty(styleAttr);
+						}
+					}
+				}
+			}
+
+			NodeUtils.setDOMAttribute(dom, attr, value);
+		}
+
+		// Add new attributes
+		for (attr in node.attrs) {
+			value = node.attrs[attr];
+
+			if (!oldNode.attrs) {
+				dom.setAttribute(attr, String(value));
+			}
+		}
+
+	} else if (prevAttrs) {
+		// Delete old attributes
+
+		for (attr in oldNode.attrs) {
+			dom.removeAttribute(attr);
+		}
+	} else if (nextAttrs) {
+		// Set new attributes
+
+		for (attr in node.attrs) {
+			value = node.attrs[attr];
+			NodeUtils.setDOMAttribute(dom, attr, value);
+		}
+	}
+}
+
+function updateNode(oldNode: VirtualNode, node: VirtualNode) {
+	switch (NodeUtils.getType(node)) {
+		case NODE_TEXT: return updateTextNode(oldNode as VirtualTextNode, node as VirtualTextNode);
+		case NODE_COMMENT: return updateCommentNode(oldNode as VirtualCommentNode, node as VirtualCommentNode);
+		// case NODE_FRAGMENT: return mountFragmentNode(node);
+		case NODE_ELEMENT: return updateElementNode(oldNode, node);
+	}
+}
+
+
+class VDom {
+
+	public append(domNode: Node, node: VirtualNode) {
+		mountNode(node);
+
+		if (NodeUtils.hasChildren(node)) {
+			const fragment = document.createDocumentFragment();
+
+			for (let i = 0; i < node.children.length; i++) {
+				this.append(fragment, node.children[i]);
+			}
+
+			node.dom.appendChild(fragment);
+		}
+
+		domNode.appendChild(node.dom);
+	}
+
+	public update(oldNode: VirtualNode, node: VirtualNode) {
+		if (oldNode === node) {
+			return;
+		}
+
+		updateNode(oldNode, node);
+	}
+
+	public remove(node: VirtualNode) {
+	}
+
+}
+
+
+function testVDomAppend(node: VirtualNode, expected: string) {
+	const root = document.createElement('I');
+	const vDom = new VDom();
+
+	vDom.append(root, node);
+
+	expect(root.innerHTML).toBe(expected);
+}
+
+function testVDomUpdate(firstNode: VirtualNode, secondNode: VirtualNode, expected: string) {
+	const root = document.createElement('I');
+	const vDom = new VDom();
+
+	vDom.append(root, firstNode);
+	vDom.update(firstNode, secondNode);
+
+	expect(root.innerHTML).toBe(expected);
+}
+
+
+describe('update', () => {
+
+	test('update same node', () => {
+		const node = {tag: 'div'};
+
+		testVDomUpdate(
+			node,
+			node,
+			'<div></div>',
+		);
+	});
+
+	test('update text node', () => {
+		testVDomUpdate(
+			{tag: '#', value: 'First'},
+			{tag: '#', value: 'Second'},
+			'Second',
+		);
+	});
+
+	test('update comment node', () => {
+		testVDomUpdate(
+			{tag: '!', value: 'First'},
+			{tag: '!', value: 'Second'},
+			'<!--Second-->',
+		);
+	});
+
+	test('update attribute', () => {
+		testVDomUpdate(
+			{tag: 'div', attrs: {'aria-disabled': false}},
+			{tag: 'div', attrs: {'aria-disabled': true}},
+			'<div aria-disabled="true"></div>',
+		);
+	});
+
+	test('update id', () => {
+		testVDomUpdate(
+			{tag: 'div', attrs: {id: 'first'}},
+			{tag: 'div', attrs: {id: 'second'}},
+			'<div id="second"></div>',
+		);
+	});
+
+	test('update class', () => {
+		testVDomUpdate(
+			{tag: 'div', attrs: {class: 'main hidden'}},
+			{tag: 'div', attrs: {class: 'main visible'}},
+			'<div class="main visible"></div>',
+		);
+	});
+
+	test('update style as string', () => {
+		testVDomUpdate(
+			{tag: 'div', attrs: {style: 'color: black;'}},
+			{tag: 'div', attrs: {style: 'opacity: 0;'}},
+			'<div style="opacity: 0;"></div>',
+		);
+	});
+
+	test('update style as object', () => {
+		testVDomUpdate(
+			{tag: 'div', attrs: {style: {color: 'black'}}},
+			{tag: 'div', attrs: {style: {opacity: '0'}}},
+			'<div style="opacity: 0;"></div>',
+		);
+	});
+
+	test('update style from string to object', () => {
+		testVDomUpdate(
+			{tag: 'div', attrs: {style: 'color: black;'}},
+			{tag: 'div', attrs: {style: {opacity: '0'}}},
+			'<div style="opacity: 0;"></div>',
+		);
+	});
+
+	test('update style from object to string', () => {
+		testVDomUpdate(
+			{tag: 'div', attrs: {style: {color: 'black'}}},
+			{tag: 'div', attrs: {style: 'opacity: 0;'}},
+			'<div style="opacity: 0;"></div>',
+		);
+	});
+
+});
+
+describe('append', () => {
+
+	test('append simple node', () => {
+		testVDomAppend(
+			{
+				tag: 'div',
+			},
+			'<div></div>',
+		);
+	});
+
+	test('append text node', () => {
+		testVDomAppend(
+			{
+				tag: '#',
+				value: 'Hello!'
+			},
+			'Hello!',
+		);
+	});
+
+	test('append comment node', () => {
+		testVDomAppend(
+			{
+				tag: '!',
+				value: 'Hello!'
+			},
+			'<!--Hello!-->',
+		);
+	});
+
+	test('append node with simple attribute', () => {
+		testVDomAppend(
+			{
+				tag: 'div',
+				attrs: {
+					contenteditable: true
+				}
+			},
+			'<div contenteditable="true"></div>',
+		);
+	});
+
+	test('append node with id', () => {
+		testVDomAppend(
+			{
+				tag: 'div',
+				attrs: {
+					id: 'main'
+				}
+			},
+			'<div id="main"></div>',
+		);
+	});
+
+	test('append node with class', () => {
+		testVDomAppend(
+			{
+				tag: 'div',
+				attrs: {
+					class: "main main-content"
+				}
+			},
+			'<div class="main main-content"></div>',
+		);
+	});
+
+	test('append node with style string', () => {
+		testVDomAppend(
+			{
+				tag: 'div',
+				attrs: {
+					style: 'min-height: 40px; transition: all 2s;'
+				}
+			},
+			'<div style="min-height: 40px; transition: all 2s;"></div>',
+		);
+	});
+
+	test('append node with style object', () => {
+		testVDomAppend(
+			{
+				tag: 'div',
+				attrs: {
+					style: {
+						minHeight: '40px',
+						border: '0 none'
+					}
+				}
+			},
+			'<div style="min-height: 40px; border: 0px none;"></div>',
+		);
+	});
+
+	test('append node with simple child', () => {
+		testVDomAppend(
+			{
+				tag: 'div',
+				children: [
+					{
+						tag: 'strong'
+					}
+				]
+			},
+			'<div><strong></strong></div>',
+		);
+	});
+
+	test('append node with multiple children', () => {
+		testVDomAppend(
+			{
+				tag: 'div',
+				children: [
+					{
+						tag: 'span',
+						attrs: {
+							style: {
+								color: 'black'
+							}
+						}
+					},
+					{
+						tag: 'strong',
+						children: [
+							{
+								tag: '#',
+								value: 'Hello!'
+							}
+						]
+					}
+				]
+			},
+			'<div><span style="color: black;"></span><strong>Hello!</strong></div>',
+		);
+	});
+
+	test('append fragment node', () => {
+		testVDomAppend(
+			{
+				children: [
+					{
+						tag: 'div',
+						attrs: {
+							style: {
+								color: 'black'
+							}
+						}
+					},
+					{
+						tag: '!',
+						value: ' value goes next '
+					},
+					{
+						tag: '#',
+						value: 'Hello!'
+					}
+				]
+			},
+			'<div style="color: black;"></div><!-- value goes next -->Hello!',
+		);
+	});
+
+
+});
